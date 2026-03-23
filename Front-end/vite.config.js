0fc578f8bc71b7ec737a7e96ea32f705dbb3c227
Mounish Arisetty <mounish@example.com>
2026-03-23 17:19:38 +0530

    production deployment setup

--- Front-end/vite.config.js
+++ Front-end/vite.config.js
@@ -1,3 +1,28 @@
+// import { defineConfig, loadEnv } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig(({ mode }) => {
+//   const env = loadEnv(mode, process.cwd(), '')
+  
+//   return {
+//     plugins: [react()],
+//     base: '/Tweet-Sentiment-Analysis/' || '/' ,
+//     server: {
+//       proxy: {
+//         '/sentiment': {
+//           // In Docker dev, use the service name 'backend'. 
+//           // Locally, use localhost.
+//           target: env.VITE_API_TARGET || 'http://localhost:8000',
+//           changeOrigin: true,
+//         },
+//       },
+//     },
+//     // This allows you to use process.env style vars if needed
+//     define: {
+//       'process.env': env
+//     }
+//   }
+// }) 
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -6,18 +31,15 @@ export default defineConfig(({ mode }) => {
   
   return {
     plugins: [react()],
-    base: '/Tweet-Sentiment-Analysis/',
+    base: env.VITE_BASE_URL || '/',
     server: {
       proxy: {
         '/sentiment': {
-          // In Docker dev, use the service name 'backend'. 
-          // Locally, use localhost.
           target: env.VITE_API_TARGET || 'http://localhost:8000',
           changeOrigin: true,
         },
       },
     },
-    // This allows you to use process.env style vars if needed
     define: {
       'process.env': env
     }
